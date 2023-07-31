3ba40074bab508ea021d58fecbeb2d79a7d19602
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2023-07-31 20:59:05 +0800

    initial exercise

---
+++ src/bin/ex3.rs
@@ -0,0 +1,9 @@
+fn main(){
+    let spaces = "  ";
+    let spaces = spaces.len();
+    println!("the value of spaces is : {spaces}");
+
+    // let mut spaces = "  "
+    // spaces = spaces.len();
+    // println!("the value of spaces is : {spaces}");
+}
