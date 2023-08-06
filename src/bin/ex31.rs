e7673b0a58a7c5d0f4d2060973f4c99027c98b86
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2023-08-06 08:59:44 +0800

    feat: add exercise 31 and 32

--- src/bin/ex31.rs
+++ src/bin/ex31.rs
@@ -8,6 +8,16 @@ impl Rectangle{
     fn area(&self)->u32{
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+impl Rectangle {
+    fn width(&self) -> bool {
+        self.width > 0 
+    }
 }
 
 fn main(){
@@ -16,8 +26,22 @@ fn main(){
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
     println!(
         "The area of the rectangle is {} square pixesls",
         rect1.area()
-    )
+    );
+
+    if rect1.width() {
+        println!(
+            "The rectangle has a nonzero witdh; it is {}",
+            rect1.width
+        )
+    }
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
 }
---
+++ src/bin/ex32.rs
@@ -0,0 +1,27 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr{
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn main() {
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback: IpAddr = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    println!("Home IP: {:?}", home.address);
+    println!("Loopback IP: {:?}", loopback.address);
+}
+
+
+
